2c9a87f2f76ca4f24b5774b15728222142e8cacc
Unknown <zezagata17@gmail.com>
2018-03-06 23:31:33 +0700

    Reconstructing Add Polygon Feature
    Polygon Fix

--- tugas-pertama-komgraf/line-and-shape/sketch.js
+++ tugas-pertama-komgraf/line-and-shape/sketch.js
@@ -1,6 +1,7 @@
 var totalLine = 1;
 var totalPoly = 1;
 let lines = new Line();
+let polygons = new Polygon();
 
 function setup() {
   var canvas = createCanvas(1200, 550);
@@ -11,17 +12,18 @@ function draw() {
   background(255);
   drawGrid();
   drawXY();
-  translate(600,275);
+  translate(600, 275);
   lines.loopHere();
+  polygons.loopHere();
 }
 
-function drawXY(){
+function drawXY() {
   strokeWeight(2);
   translate(600, 0);
   line(0, 0, 0, 550)
-  translate(-600,275);
-  line(0,0,1200,0);
-  translate(0,-275);
+  translate(-600, 275);
+  line(0, 0, 1200, 0);
+  translate(0, -275);
 }
 
 function drawGrid() {
@@ -63,8 +65,8 @@ function addPolygon() {
   }
   var inputHtml = "";
   for (var i = 1; i <= vertextTotal; i++) {
-    inputHtml += '<input id="poly-x1-' + i + '" type="number" name="" value="" placeholder="Coordinate x' + i + '">';
-    inputHtml += '<input id="poly-y1-' + i + '" type="number" name="" value="" placeholder="Coordinate y' + i + '">';
+    inputHtml += '<input id="poly-x' + i + '-' + totalPoly + '" type="number" name="" value="" placeholder="Coordinate x' + i + '">';
+    inputHtml += '<input id="poly-y' + i + '-' + totalPoly + '" type="number" name="" value="" placeholder="Coordinate y' + i + '">';
   }
   var cardHtml = '<h2 class="card-caption">Polygon ' + totalPoly + '</h2>' + '<div class="separator-card">' + '</div>' + '<div class="polygon-input-container flex">' + inputHtml + '</div>' + '<div class="separator-card">' + '</div>' + '<button id="poly-' + totalPoly + '" type="button" class="btn active btn-custom" onclick="drawPoly(this.id)"><code class="button-caption">DRAW</code></button>';
   var parent = document.getElementById('item-container');
@@ -85,11 +87,11 @@ function drawLine(id) {
   let _y1 = document.getElementById('line-y1-' + lineId[1]);
   let _x2 = document.getElementById('line-x2-' + lineId[1]);
   let _y2 = document.getElementById('line-y2-' + lineId[1]);
-  _x1.value = _x1.value % 600;
-  _y1.value = _y1.value % 275;
-  _x2.value = _x2.value % 600;
-  _y2.value = _y2.value % 275;
   if (handlerLineInput(_x1.value, _y1.value, _x2.value, _y2.value)) {
+    _x1.value = _x1.value % 600;
+    _y1.value = _y1.value % 275;
+    _x2.value = _x2.value % 600;
+    _y2.value = _y2.value % 275;
     lines.add(lineId[1], _x1.value, _y1.value, _x2.value, _y2.value);
   }
 }
@@ -98,5 +100,27 @@ function drawLine(id) {
   Function to draw a polygon in canvas
 */
 function drawPoly(id) {
-  alert(id);
+  const polyId = id.split("-");
+  var vertextList = new Array();
+  var counter = 0;
+  while (true) {
+    counter += 1;
+    let tempX = document.getElementById('poly-x' + counter + '-' + polyId[1]);
+    let tempY = document.getElementById('poly-y' + counter + '-' + polyId[1]);
+    if (tempX === null) {
+      counter -= 1;
+      break;
+    } else {
+      if (tempX.value === "" || tempY.value === "") {
+        alert("Input harus terisi semua !");
+        return;
+      } else {
+        tempX.value = tempX.value % 600;
+        tempY.value = tempY.value % 275;
+        vertextList.push([tempX.value, tempY.value]);
+      }
+    }
+  }
+  polygons.add(polyId[1],vertextList);
+  polygons.test();
 }
